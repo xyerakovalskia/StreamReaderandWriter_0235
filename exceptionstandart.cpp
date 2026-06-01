3a268be50fc36753991758aadfd7bca570215e70
xyerakovalskia <xyera123987@gmail.com>
2026-06-01 18:06:42 +0700

    deklarasi array data panjang index 3

--- exceptionstandart.cpp
+++ exceptionstandart.cpp
@@ -7,4 +7,6 @@ int main()
 {
     cout << "Awal program" << endl; //penanda 1
     try
-    
+    {
+        //deklarasi array data dengan panjang index 3
+        array <int, 3> data = { 1, 2, 3 };
