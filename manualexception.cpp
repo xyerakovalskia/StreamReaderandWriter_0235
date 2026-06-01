9a2bbd1df5ccfb60435b8cb3b2c91c48bd2e3dc4
xyerakovalskia <xyera123987@gmail.com>
2026-06-01 18:23:28 +0700

    membuat isi dari try utk pengujian eror

--- manualexception.cpp
+++ manualexception.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
