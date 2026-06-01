dbde23d3439a4e7292a5fee8b799aad01b76320a
xyerakovalskia <xyera123987@gmail.com>
2026-06-01 18:14:39 +0700

    membuat file mode menulis dan menunjuk nama file dengan txt

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -11,3 +11,10 @@ int main()
     //membuat input untuk menangkap nama file
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
